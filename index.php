09534a82b9b2cc54238f1c1837b9d0869dd05073
j35u54n6u10 <j35u54n6u10@gmail.com>
2024-11-26 23:56:11 -0400

    NavBar 1.0

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo "!!";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>navbar Uno</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="assets/loguito.jpeg" alt="Logo de la marca">
+        </div>
+        <nav>
+           <ul class="nav-links">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Acerca de</a></li>
+           </ul>            
+        </nav>
+        <a class="btn" href="#"><button>Contáctanos</button></a>
+    </header>
+</body>
+</html>
