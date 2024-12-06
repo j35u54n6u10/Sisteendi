9e02355e6212151bca16d360032c290ae99d582b
j35u54n6u10 <j35u54n6u10@gmail.com>
2024-12-06 15:27:38 -0400

    Add banner design

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <header class="header">
         <div class="logo">
-<!---->            <img src="assets/loguito.png" alt="Logo de la marca">
+            <img src="assets/loguito.png" alt="Logo de la marca">
         </div>
         <nav>
            <ul class="nav-links">
@@ -21,22 +21,33 @@
         </nav>
         <a class="btn" href="#"><button>Iniciar Sesión</button></a>
 
-<!---->        <a onclick="openNav()" class="menu" href="#"><button>Menu</button></a>
+        <a onclick="openNav()" class="menu" href="#"><button>Menu</button></a>
 
-<!---->        <div id="mobile-menu" class="overlay">
-<!---->            <a onclick="closeNav()" href="" class="close">&times;</a>
-<!---->            <div class="overlay-content">
-<!---->                <a href="#">Inicio</a>
-<!---->                <a href="#">Productos</a>
-<!---->                <a href="#">Acerca de</a>
-<!---->                <a href="#">Iniciar Sesión</a>
-<!---->            </div>
-<!---->        </div>
+        <div id="mobile-menu" class="overlay">
+            <a onclick="closeNav()" href="" class="close">&times;</a>
+            <div class="overlay-content">
+                <a href="#">Inicio</a>
+                <a href="#">Productos</a>
+                <a href="#">Acerca de</a>
+                <a href="#">Iniciar Sesión</a>
+            </div>
+        </div>
 
     </header>
 
+    <section>
+        <div>
+            <h1>Dale un gustico</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere molestiae, 
+                repellendus quasi assumenda cumque harum. Quis sapiente hic dicta laboriosam!
+            </p>
+            <img src="" alt="">
+            
+        </div>
+    </section>
 
-<!---->    <script type="text/javascript" src="js/nav.js"></script>
+
+    <script type="text/javascript" src="js/nav.js"></script>
 
 </body>
 </html>
