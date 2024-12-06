f488c658a9c237ad5f65e9b7155594f729e67146
j35u54n6u10 <j35u54n6u10@gmail.com>
2024-12-05 20:39:02 -0400

    Navbar 1.2

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>navbar Uno</title>
+    <title>Sisteendi</title>
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <header class="header">
         <div class="logo">
-            <img src="assets/loguito.jpeg" alt="Logo de la marca">
+<!---->            <img src="assets/loguito.png" alt="Logo de la marca">
         </div>
         <nav>
            <ul class="nav-links">
@@ -19,7 +19,24 @@
                 <li><a href="#">Acerca de</a></li>
            </ul>            
         </nav>
-        <a class="btn" href="#"><button>Contáctanos</button></a>
+        <a class="btn" href="#"><button>Iniciar Sesión</button></a>
+
+<!---->        <a onclick="openNav()" class="menu" href="#"><button>Menu</button></a>
+
+<!---->        <div id="mobile-menu" class="overlay">
+<!---->            <a onclick="closeNav()" href="" class="close">&times;</a>
+<!---->            <div class="overlay-content">
+<!---->                <a href="#">Inicio</a>
+<!---->                <a href="#">Productos</a>
+<!---->                <a href="#">Acerca de</a>
+<!---->                <a href="#">Iniciar Sesión</a>
+<!---->            </div>
+<!---->        </div>
+
     </header>
+
+
+<!---->    <script type="text/javascript" src="js/nav.js"></script>
+
 </body>
 </html>
